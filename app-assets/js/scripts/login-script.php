6e597fe7834bdfcd4e1b7a31a6e44832af8b0ed4
Osotechcoding <osotechcoding@gmail.com>
2022-06-29 19:44:43 +0100

    All files was deleted

--- app-assets/js/scripts/login-script.php
+++
@@ -1,8 +0,0 @@
- <script>
-    $(document).ready(function(){
-    $("#login-Form").on("submit", function(event){
-    event.preventDefault();
-    alert("<?php echo $lang['login_error1'];?>");
-            });
-        })
-    </script>
